8a8c08261a835baf0c5e08b32575d3e0c2388a76
ypreiger <ypreiger@redhat.com>
2019-12-02 22:54:35 +0200

    Update xml_validator.lua

--- policies/validinput/0.1/xml_validator.lua
+++ policies/validinput/0.1/xml_validator.lua
@@ -6,8 +6,15 @@ local xmllib = require('xml2lua')
 --Uses a handler that converts the XML to a Lua table
 local handler = require("xmlhandler.tree")
 
+local setmetatable = setmetatable
 local _M = { }
+local mt = { __index = _M }
 
+function _M.new()
+  local self = setmetatable({}, mt)
+  ngx.log(ngx.WARN, "=========>>>>>NEW XML VALIDATOR")  
+  return self
+end
 --- Validate an XML object
 -- Checks if XML content is valid according to the given schema.
 -- @tparam table xml_content XML content
