825db3b4a062dc2a376619c3aa375281a45f8f2e
ypreiger <ypreiger@redhat.com>
2019-12-02 19:12:36 +0200

    Create json_validator.lua

---
+++ policies/validinput/0.1/json_validator.lua
@@ -0,0 +1,19 @@
+--- JSON Validator
+-- Validates a JSON against a schema.
+
+-- local jsonschema = require('jsonschema')
+
+local _M = { }
+
+--- Validate a JSON object
+-- Checks if JSON content is valid according to the given schema.
+-- @tparam table json_content JSON content
+-- @tparam table config_schema JSON schema
+-- @treturn boolean True if the JSON is valid. False otherwise.
+-- @treturn string Error message only when the JSON is invalid.
+function _M.validate(json_content, config_schema)
+  -- local validator = jsonschema.generate_validator(config_schema or {})
+  -- return validator(json_content or {})
+end
+
+return _M
