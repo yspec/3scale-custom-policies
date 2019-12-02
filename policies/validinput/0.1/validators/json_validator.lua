45e59361246fbef274fac2449362c9156cf2a3a9
ypreiger <ypreiger@redhat.com>
2019-12-03 01:00:58 +0200

    Update json_validator.lua

--- policies/validinput/0.1/validators/json_validator.lua
+++ policies/validinput/0.1/validators/json_validator.lua
@@ -24,7 +24,7 @@ function _M.validate(json_content)
       ngx.log(ngx.WARN, "json_content: ", json_content)
 
   -- local validator = jsonschema.generate_validator(config_schema or {})
-  return jsonvalidator.validate(json_content) --validator(json_content or {})
+  return jsonvalidator.isValid(json_content) --validator(json_content or {})
 end
 
 function _M.validateschema(json_content, config_schema)
